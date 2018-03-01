bf5250b9fd8e3f5c8dcbd785e368dd16f4d08ecb
yx <xyang@163.com>
2018-03-01 10:06:06 +0800

    new

---
+++ src/main/java/com/github/jlf1997/tools/time/TimeUtil.java
@@ -0,0 +1,15 @@
+package com.github.jlf1997.tools.time;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtil {
+
+	 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public static Date strToDate(String str) {
+		
+		return null;
+		
+	}
+}
